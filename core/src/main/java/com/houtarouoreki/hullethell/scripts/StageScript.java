1efe6d2e66e561194ceaa88a27690d3455b65469
Hōtarō Oreki <TheZjarek@gmail.com>
2020-11-09 05:03:59 +0100

    small changes

--- core/src/main/java/com/houtarouoreki/hullethell/scripts/StageScript.java
+++ core/src/main/java/com/houtarouoreki/hullethell/scripts/StageScript.java
@@ -7,12 +7,12 @@ public class StageScript {
     public List<ScriptedEntity> entities = new ArrayList<ScriptedEntity>();
 
     private void readLines(List<String> lines) {
-        ScriptedEntity currentEntity;
+        ScriptedEntity currentEntity = null;
         for (String line : lines) {
             if (line.contains("=")) {
                 currentEntity = new ScriptedEntity();
-            } else if (line.startsWith("\t")) {
-                ScriptedAction action = ScriptedAction.createScriptedAction(line);
+            } else if (line.startsWith("\t") && currentEntity != null) {
+                currentEntity.actions.add(ScriptedAction.createScriptedAction(line));
             }
         }
     }
--- core/src/main/java/com/houtarouoreki/hullethell/scripts/actions/ShootAction.java
+++ core/src/main/java/com/houtarouoreki/hullethell/scripts/actions/ShootAction.java
@@ -15,6 +15,5 @@ public class ShootAction extends ScriptedAction {
 
     @Override
     public void physics() {
-
     }
 }
