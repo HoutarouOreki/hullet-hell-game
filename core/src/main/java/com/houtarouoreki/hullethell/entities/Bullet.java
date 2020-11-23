2294f06c729f3a649a9b080e52477b54e702cc0d
Hōtarō Oreki <TheZjarek@gmail.com>
2020-11-23 17:06:44 +0100

    Remove commented out Bullet color code

--- core/src/main/java/com/houtarouoreki/hullethell/entities/Bullet.java
+++ core/src/main/java/com/houtarouoreki/hullethell/entities/Bullet.java
@@ -1,12 +1,9 @@
 package com.houtarouoreki.hullethell.entities;
 
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 import com.houtarouoreki.hullethell.configurations.BodyConfiguration;
 import com.houtarouoreki.hullethell.environment.collisions.CollisionResult;
-
-import java.util.Random;
 
 public class Bullet extends Entity {
     private Ship source;
@@ -20,40 +17,6 @@ public class Bullet extends Entity {
         setSize(new Vector2(c.getSize()));
         setCollisionBody(c.getCollisionCircles());
         setShouldDespawnOOBounds(true);
-//        Color color;
-//        switch (new Random().nextInt(10)) {
-//            case 0:
-//                color = Color.RED;
-//                break;
-//            case 1:
-//                color = Color.YELLOW;
-//                break;
-//            case 2:
-//                color = Color.GREEN;
-//                break;
-//            case 3:
-//                color = Color.CYAN;
-//                break;
-//            case 4:
-//                color = Color.BLUE;
-//                break;
-//            case 5:
-//                color = Color.PINK;
-//                break;
-//            case 6:
-//                color = Color.PURPLE;
-//                break;
-//            case 7:
-//                color = Color.CORAL;
-//                break;
-//            case 8:
-//                color = Color.GOLD;
-//                break;
-//            default:
-//                color = Color.MAGENTA;
-//                break;
-//        }
-//        sprite.setColor(color);
     }
 
     @Override
