9b83a091f19a391f040341a49aa84a1da428a55b
Hōtarō Oreki <TheZjarek@gmail.com>
2020-12-09 08:27:48 +0100

    Add key handling draft

--- core/src/main/java/com/houtarouoreki/hullethell/ui/Menu.java
+++ core/src/main/java/com/houtarouoreki/hullethell/ui/Menu.java
@@ -1,12 +1,14 @@
 package com.houtarouoreki.hullethell.ui;
 
 
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import org.mini2Dx.core.graphics.Graphics;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Menu {
+public class Menu implements InputProcessor {
     public final List<MenuComponent> components;
 
     public Menu() {
@@ -23,5 +25,73 @@ public class Menu {
         for (MenuComponent component : components) {
             component.render(g);
         }
+    }
+
+    public MenuComponent getCurrentlyFocusedComponent() {
+        for (MenuComponent component : components) {
+            if (component.isCurrentlyFocused())
+                return component;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        MenuComponent focusedComponent = getCurrentlyFocusedComponent();
+        if (components.size() == 0)
+            return false;
+        if (keycode == Input.Keys.TAB) {
+            handleTabKey(focusedComponent);
+            return true;
+        }
+        return focusedComponent.handleKey(keycode);
+    }
+
+    private void handleTabKey(MenuComponent focusedComponent) {
+        if (components.size() <= 1)
+            return;
+        if (focusedComponent == null) {
+            components.get(0).setCurrentlyFocused(true);
+            return;
+        }
+        int indexFocused = components.indexOf(focusedComponent);
+        if (indexFocused == components.size() - 1)
+            components.get(0).setCurrentlyFocused(true);
+        focusedComponent.setCurrentlyFocused(false);
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
     }
 }
--- core/src/main/java/com/houtarouoreki/hullethell/ui/MenuComponent.java
+++ core/src/main/java/com/houtarouoreki/hullethell/ui/MenuComponent.java
@@ -2,10 +2,22 @@ package com.houtarouoreki.hullethell.ui;
 
 import org.mini2Dx.core.graphics.Graphics;
 
-public class MenuComponent {
+public abstract class MenuComponent {
+    private boolean currentlyFocused;
+
     public void update(float delta) {
     }
 
     public void render(Graphics g) {
     }
+
+    public boolean isCurrentlyFocused() {
+        return currentlyFocused;
+    }
+
+    public void setCurrentlyFocused(boolean value) {
+        currentlyFocused = value;
+    }
+
+    public abstract boolean handleKey(int key);
 }
