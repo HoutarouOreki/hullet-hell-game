4fcc0962e43dbb9cd2896a463f87cb7fccdf9edf
Hōtarō Oreki <TheZjarek@gmail.com>
2020-12-17 15:33:35 +0100

    Implement DialogueBox redesign (closes #26), add talking character name and avatar display (#24)

--- core/src/main/java/com/houtarouoreki/hullethell/graphics/DialogueBox.java
+++ core/src/main/java/com/houtarouoreki/hullethell/graphics/DialogueBox.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Vector2;
+import com.houtarouoreki.hullethell.HulletHellGame;
+import com.houtarouoreki.hullethell.bindables.BindableNumber;
 import com.houtarouoreki.hullethell.helpers.BasicListener;
 import com.houtarouoreki.hullethell.input.ControlProcessor;
 import com.houtarouoreki.hullethell.input.Controls;
@@ -12,8 +14,12 @@ import com.houtarouoreki.hullethell.ui.Label;
 import java.util.EnumSet;
 
 public class DialogueBox extends Drawable implements ControlProcessor {
-    private final Label label;
+    private final Label text;
     private final float size;
+    private final Sprite characterSprite;
+    private final Label characterName;
+    private final float characterX = 100;
+    private final float transitionsLength = 1;
     String fullText = "";
     private float timePassed;
     private float maxDuration;
@@ -23,7 +29,7 @@ public class DialogueBox extends Drawable implements ControlProcessor {
     private float liveTime;
 
     public DialogueBox() {
-        size = 300;
+        size = 200;
 
         setRelativeSizeAxes(EnumSet.of(Axes.HORIZONTAL));
         setOrigin(new Vector2(0.5f, 1));
@@ -31,31 +37,42 @@ public class DialogueBox extends Drawable implements ControlProcessor {
         setSize(new Vector2(1, size));
         setPosition(new Vector2(0, size + 60));
 
-        Rectangle backgroundRect = new Rectangle();
-        backgroundRect.setColor(new Color(0.1f, 0.1f, 0.3f, 0.6f));
-        backgroundRect.setRelativeSizeAxes(EnumSet.allOf(Axes.class));
-        add(backgroundRect);
+        Sprite background = new Sprite();
+        background.texture = HulletHellGame.getAssetManager().get("ui/dialogueOverlay.png");
+        background.setRelativeSizeAxes(EnumSet.allOf(Axes.class));
+        add(background);
 
-        BorderRectangle fullBorder = new BorderRectangle();
-        fullBorder.setRelativeSizeAxes(EnumSet.allOf(Axes.class));
-        add(fullBorder);
+        Container textContainer = new Container();
+        textContainer.setPadding(new PaddingMargin(
+                size * 0.46f, 50, 150, 100));
+        textContainer.setRelativeSizeAxes(EnumSet.allOf(Axes.class));
+        add(textContainer);
 
-        BorderRectangle avatarBorder = new BorderRectangle();
-        avatarBorder.setSize(new Vector2(size, size));
-        add(avatarBorder);
-
-        Container labelContainer = new Container();
-        labelContainer.setPadding(new PaddingMargin(50, 50, size + 50, 50));
-        labelContainer.setRelativeSizeAxes(EnumSet.allOf(Axes.class));
-        add(labelContainer);
-
-        label = new Label();
-        label.setRelativeSizeAxes(EnumSet.allOf(Axes.class));
+        text = new Label();
+        text.setRelativeSizeAxes(EnumSet.allOf(Axes.class));
         FreeTypeFontGenerator.FreeTypeFontParameter fontParameters
                 = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        fontParameters.size = 42;
-        label.font = Fonts.getFont("acme", fontParameters);
-        labelContainer.add(label);
+        fontParameters.size = 26;
+        text.font = Fonts.getFont("acme", fontParameters);
+        textContainer.add(text);
+
+        Container characterNameContainer = new Container();
+        characterNameContainer.setPosition(new Vector2(150, 32));
+        characterNameContainer.setSize(new Vector2(500, 50));
+        add(characterNameContainer);
+
+        characterName = new Label();
+        fontParameters.size = 34;
+        fontParameters.borderWidth = 1;
+        fontParameters.borderColor = Color.BLACK;
+        characterName.font = Fonts.getFont("acme", fontParameters);
+        characterNameContainer.add(characterName);
+
+        characterSprite = new Sprite();
+        characterSprite.setSize(new Vector2(160, 160));
+        characterSprite.setOrigin(new Vector2(0, 1));
+        characterSprite.setPosition(new Vector2(characterX, 0));
+        add(characterSprite);
     }
 
     @Override
@@ -76,27 +93,54 @@ public class DialogueBox extends Drawable implements ControlProcessor {
 
     private void animatePopIn() {
         float since = liveTime - appearTime;
-        float length = 0.5f;
-        float a = since / length;
-        a = Math.min(a, 1);
-        float y = Interpolation.sineOut.apply(getSize().y + 60, 0, a);
+        BindableNumber<Float> a
+                = new BindableNumber<Float>(since / transitionsLength, 0f, 1f);
+        BindableNumber<Float> vertical
+                = new BindableNumber<Float>(a.getValue() * 2, 0f, 1f);
+        BindableNumber<Float> horizontal
+                = new BindableNumber<Float>(a.getValue() * 2 - 1, 0f, 1f);
+        float y = Interpolation.sineOut
+                .apply(getSize().y + 60, 0, vertical.getValue());
         setPosition(new Vector2(0, y));
+        float x = Interpolation.sineOut
+                .apply(-characterSprite.getSize().x, characterX, horizontal.getValue());
+        characterSprite.setPosition(new Vector2(x, 0));
     }
 
     private void animatePopOut() {
         float since = liveTime - disappearTime;
-        float length = 0.5f;
-        float a = since / length;
-        a = Math.min(a, 1);
-        float y = Interpolation.sineIn.apply(0, getSize().y + 60, a);
+        BindableNumber<Float> a
+                = new BindableNumber<Float>(since / transitionsLength, 0f, 1f);
+        BindableNumber<Float> vertical
+                = new BindableNumber<Float>(a.getValue() * 2 - 1, 0f, 1f);
+        BindableNumber<Float> horizontal
+                = new BindableNumber<Float>(a.getValue() * 2, 0f, 1f);
+        float y = Interpolation.sineIn
+                .apply(0, getSize().y + 60, vertical.getValue());
         setPosition(new Vector2(0, y));
+        float x = Interpolation.sineIn
+                .apply(characterX, -characterSprite.getSize().x, horizontal.getValue());
+        characterSprite.setPosition(new Vector2(x, 0));
     }
 
     private void revealText() {
         float charsPerSecond = 32;
         int shownChars = (int) (timePassed * charsPerSecond);
         String shownText = fullText.substring(0, Math.min(shownChars, fullText.length()));
-        label.setText(shownText);
+        text.setText(shownText);
+    }
+
+    public void setCharacter(String name) {
+        if (name == null || name.equals("")) {
+            characterSprite.setVisibility(false);
+            characterName.setVisibility(false);
+        } else {
+            characterSprite.setVisibility(true);
+            characterName.setVisibility(true);
+            characterName.setText(name);
+            characterSprite.texture = HulletHellGame.getAssetManager()
+                    .get("characters/" + name + ".png");
+        }
     }
 
     public void showText(String text, float maxDuration, BasicListener onFinish) {
@@ -104,6 +148,7 @@ public class DialogueBox extends Drawable implements ControlProcessor {
             appearTime = liveTime;
         else if (!fullText.equals(""))
             disappearTime = liveTime;
+
         fullText = text;
         timePassed = 0;
         this.maxDuration = maxDuration;
@@ -132,6 +177,8 @@ public class DialogueBox extends Drawable implements ControlProcessor {
         disappearTime = 0;
         fullText = "";
         liveTime = 0;
+        characterSprite.setPosition(new Vector2(-characterSprite.getSize().x, 0));
         setPosition(new Vector2(0, size + 60));
+        setCharacter(null);
     }
 }
