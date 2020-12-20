0b91381cda80c7ab90493565cd737d60b4811795
Hōtarō Oreki <TheZjarek@gmail.com>
2020-12-20 20:23:28 +0100

    Items rotate passively

--- core/src/main/java/com/houtarouoreki/hullethell/entities/Item.java
+++ core/src/main/java/com/houtarouoreki/hullethell/entities/Item.java
@@ -2,9 +2,13 @@ package com.houtarouoreki.hullethell.entities;
 
 import com.badlogic.gdx.math.Vector2;
 import org.mini2Dx.core.engine.geom.CollisionCircle;
+import org.mini2Dx.core.graphics.Sprite;
+
+import java.util.Random;
 
 public class Item extends Body {
     public final String name;
+    private final boolean rotatingClockwise;
 
     public Item(String itemName) {
         name = itemName;
@@ -12,5 +16,13 @@ public class Item extends Body {
         setSize(new Vector2(1, 1));
         setTextureName("items/" + itemName + ".png");
         setVelocity(new Vector2(-1, 0));
+        rotatingClockwise = new Random().nextBoolean();
+    }
+
+    public void update(float delta) {
+        super.update(delta);
+        for (Sprite sprite : sprites) {
+            sprite.rotate(rotatingClockwise ? 1 : -1);
+        }
     }
 }
