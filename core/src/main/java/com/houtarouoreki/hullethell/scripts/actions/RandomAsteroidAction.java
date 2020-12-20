5b18bcc766128099574bb8f6784c07717f87d634
Hōtarō Oreki <TheZjarek@gmail.com>
2020-12-20 04:40:12 +0100

    Fix RandomAsteroidAction

--- core/src/main/java/com/houtarouoreki/hullethell/scripts/actions/RandomAsteroidAction.java
+++ core/src/main/java/com/houtarouoreki/hullethell/scripts/actions/RandomAsteroidAction.java
@@ -2,6 +2,7 @@ package com.houtarouoreki.hullethell.scripts.actions;
 
 import com.badlogic.gdx.math.Vector2;
 import com.houtarouoreki.hullethell.entities.Environmental;
+import com.houtarouoreki.hullethell.environment.World;
 import com.houtarouoreki.hullethell.scripts.ScriptedAction;
 
 import java.util.Arrays;
@@ -15,21 +16,28 @@ public class RandomAsteroidAction extends ScriptedAction {
         super.initialiseArguments();
         Random random = new Random();
         Vector2 velocity = new Vector2(
-                random.nextFloat() * 6 + 3,
+                -(random.nextFloat() * 6 + 3),
                 random.nextFloat() * 2.2f - 1.1f
         );
         Vector2 startingPosition = new Vector2(
                 1.2f,
                 random.nextFloat()
-        );
+        ).scl(World.viewArea);
         if (arguments.size() == 0)
             return;
 
         String[] itemStrings = arguments.get(0).split(" / ");
 
+        float scale = random.nextFloat() * 2 + 1;
+
         asteroid = new Environmental("Asteroid");
+        asteroid.setShouldDespawnOOBounds(false);
+        asteroid.setHealth(10);
         asteroid.setVelocity(velocity);
         asteroid.setPosition(startingPosition);
+        asteroid.setSize(asteroid.getSize().scl(scale));
+        asteroid.getCollisionBody().get(0)
+                .setRadius(asteroid.getCollisionBody().get(0).getRadius() * scale);
         asteroid.itemDrops.addAll(Arrays.asList(itemStrings));
     }
 
@@ -37,9 +45,8 @@ public class RandomAsteroidAction extends ScriptedAction {
     protected void performAction() {
         if (getTicks() == 0)
             world.addBody(asteroid);
-        if (asteroid.getPosition().x < -0.2 || !asteroid.isAlive()) {
+        else if (asteroid.getPosition().x < -0.2 || !asteroid.isAlive())
             setFinished();
-        }
     }
 
     @Override
