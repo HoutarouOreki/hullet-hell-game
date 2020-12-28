de226f313cd771c26c36d57b974f9190d59d952c
Hōtarō Oreki <TheZjarek@gmail.com>
2020-12-28 22:41:32 +0100

    Add thickness field to BorderRectangle

--- core/src/main/java/com/houtarouoreki/hullethell/graphics/BorderRectangle.java
+++ core/src/main/java/com/houtarouoreki/hullethell/graphics/BorderRectangle.java
@@ -3,9 +3,11 @@ package com.houtarouoreki.hullethell.graphics;
 import org.mini2Dx.core.graphics.Graphics;
 
 public class BorderRectangle extends Drawable {
+    public int thickness = 3;
+
     @Override
     public void draw(Graphics g) {
-        g.setLineHeight(3);
+        g.setLineHeight(thickness);
         g.drawRect(getRenderPosition().x, getRenderPosition().y,
                 getRenderSize().x, getRenderSize().y);
     }
